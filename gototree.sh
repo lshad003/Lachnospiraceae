a3ea3e4cea9d3701226a2cb36608954fe65289b8
Leila Shadmani <108957126+lshad003@users.noreply.github.com>
2024-07-10 21:34:42 -0700

    Create gototree.sh

---
+++ gototree.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash -l
+#SBATCH -p batch --mem 128gb -N 1 -n 1 -c 32  --out logs/gtotree.%A.log
+
+module load gtotree
+
+
+# Run GToTree
+GToTree -a edited_assembly_result_4.txt -f my_Lachnospiraceae.txt -H /opt/linux/rocky/8.x/x86_64/pkgs/gtotree/1.8.1/share/gtot
+ree/hmm_sets/Firmicut
+es.hmm -t -L Species -j 4 -o Syn-GToTree_6-out
